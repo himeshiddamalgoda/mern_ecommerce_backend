45e33834c3bb692fbb9308fbaa023f0e5d50bd74
himeshiddamalgoda <himeshiddamalgoda@gmail.com>
2021-08-04 15:02:09 +0530

    server correction

--- server.js
+++ server.js
@@ -49,14 +49,7 @@ app.use('/api/users', userRoute)
 
 app.use('/api/orders', orderRoute)
 
-if(process.env.NODE_ENV === 'production')
-{
-    app.use('/' , express.static('client/build'))
 
-    app.use('*' , (req,res) => {
-        res.sendFile(path.resolve(__dirname , 'client/build/index.html'))
-    })
-}
 
 
 // listen
